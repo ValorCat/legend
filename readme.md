8cd863ec07f8948166dae312e4d9e7f6cef029a2
ValorCat <amorrell314@gmail.com>
2019-10-15 20:32:17 -0400

    Overhaul interpreter's commandline interface

--- readme.md
+++ readme.md
@@ -4,37 +4,66 @@ Legend is a WIP multi-purpose programming language that is intended to be intuit
 Here's an example of a simple program:
 
 ```
-"What's your name?"
-name = String.read()
+name = read("What's your name? ")
 if #name == 0
-    "You must enter a name!"
-else if name == 'ValorCat'
-    "I doubt that."
+    >> "You must enter a name!"
+else if name == "ValorCat"
+    >> "I doubt that."
 else
-    ('Hello, ' & name).show()
+    >> "Hello, " & name
 end
 ```
 
 Checkout the wiki for a syntax reference: https://github.com/ValorCat/legend/wiki
 
-## Running the Interpreter
+## Using the Interpreter
+The syntax for running the interpeter is:
+
+```shell script
+legend [-sw] <source-file> [args...]
+```
+
+#### Interpreter Flags
+Interpreter flags are special directives to the interpreter.
+
+| Flag | Meaning | Description |
+|------|---------|-------------|
+| `-s` | Strict Typing | Raises an error on encountering an untyped variable. |
+| `-w` | Wait on Exit | Keeps the window open after the program terminates until the user presses any key. |
+
+#### Source File
+The path to the source file. The file must have the `.leg` extension, though it doesn't need to be specified. Both of the following are valid:
+
+```
+./legend input
+./legend input.leg
+```
+
+#### Program Arguments
+Arguments to pass to the program. These can be accessed within the program from the `args` variable:
+
+```
+for a in args
+    >> a
+end
+```
+
+## Interpreter Versions
 There are two versions of the interpreter. Read below to determine which version is best for you.
 
 ### Standalone Version
-This version is slightly larger (~25 mb unzipped), but has no external dependencies. Simply go to the releases page and choose the latest version:
+This version is slightly larger (~25mb), but has no external dependencies. Simply go to the releases page and choose the latest version:
 
 **Releases:** https://github.com/ValorCat/legend/releases
 
-At the bottom under "Assets", download `legend-standalone.zip`. Unzip it and navigate to the `bin` subdirectory. You can run the interpreter by double-clicking `legend.bat` (on Windows) or `legend` (on Mac/Linux).
+At the bottom under "Assets", download `legend-standalone.zip` and unzip it. The start script is at `./bin/legend`:
 
-You can also run the interpreter from the terminal with:
-
-```
-./bin/legend <source-file>
+```shell script
+./bin/legend <source-file> [args...]
 ```
 
 ### Java-Dependent Version
-This version is much smaller (~200 kb), but requires Java 11 installed. If you don't have Java 11 but want to use this version, you can download the Java 11 JDK from Oracle:
+This version is much smaller (<1mb), but requires Java 11 installed. If you don't have Java 11 but want to use this version, you can download the Java 11 JDK from Oracle:
 
 **JDK 11:** https://www.oracle.com/technetwork/java/javase/downloads/jdk11-downloads-5066655.html
 
@@ -42,8 +71,12 @@ Once Java is installed, go to the Legend releases page and choose the latest ver
 
 **Releases:** https://github.com/ValorCat/legend/releases
 
-At the bottom under "Assets", download `legend.jar`. On some systems, you can double-click the JAR file to run it. Otherwise, you can run the interpreter from the terminal with:
+At the bottom under "Assets", download `legend-java.zip` and unzip it. You can run the interpeter with the provided start script if Java is on the system path, or manually otherwise:
 
+```shell script
+./legend <source-file> [args...]
 ```
-java -jar legend.jar <source-file>
+
+```shell script
+path/to/java -jar legend.jar <source-file> [args...]
 ```
--- src/legend/Interpreter.java
+++ src/legend/Interpreter.java
@@ -12,17 +12,16 @@ import legend.compiletime.statement.Statement;
 import legend.runtime.Program;
 import legend.runtime.instruction.Instruction;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * This class is the public entry point of the interpreter. You can execute a Legend file by calling
- * {@link #interpret(File)} and passing in the file object.
+ * This class is the public entry point of the interpreter.
  *
  * Interpretation is a 4-step process:
  *
@@ -38,61 +37,92 @@ import java.util.Scanner;
  */
 public class Interpreter {
 
+    private static final String USAGE =
+            "Usage: legend [flags...] <source-file> [args...]\n" +
+            "  Flags: -s (strict typing), -w (wait on exit)\n" +
+            "  Source File: path to .leg source file (may omit file extension)\n" +
+            "  Args: user-defined program arguments";
+
+    private static boolean strictTyping = false;    // -s flag
+    private static boolean waitOnExit = false;      // -w flag
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String sourcePath = "";
         if (args.length == 0) {
-            System.out.print("Enter the path to the source file: ");
-            sourcePath = scanner.nextLine();
-        } else if (args.length > 1) {
-            System.err.println("Usage: ./bin/legend <source-file>          (standalone interpreter)"
-                    .replace('/', File.separatorChar));
-            System.err.println("Usage: java -jar legend.jar <source-file>  (java-dependent interpreter)");
-        } else {
-            sourcePath = args[0];
+            System.out.println(USAGE);
+            return;
         }
-        if (!sourcePath.isEmpty()) {
-            if (!sourcePath.endsWith(".leg")) {
-                sourcePath += ".leg";
-            }
-            Path path = Paths.get(sourcePath);
-            if (Files.exists(path) && !Files.isDirectory(path)) {
-                interpret(path.toFile());
+
+        Path sourcePath = parseArguments(args);
+        if (sourcePath != null) {
+            Path absolutePath = sourcePath.toAbsolutePath();
+            if (Files.isRegularFile(sourcePath)) {
+                try {
+                    interpret(Files.readString(sourcePath));
+                } catch (IOException e) {
+                    System.err.printf("Cannot read source file at %s: %s\n", absolutePath, e.getMessage());
+                }
             } else {
-                System.err.println("Cannot find source file: " + sourcePath);
+                System.err.println("Cannot find source file: " + absolutePath);
             }
         }
-        System.out.print("\nPress enter to exit...");
-        scanner.nextLine();
-        scanner.close();
+
+        if (waitOnExit) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Press enter to exit...");
+            scanner.nextLine();
+            scanner.close();
+        }
     }
 
     /**
-     * Execute a Legend source file.
-     * @param sourceFile the file to execute
+     * Parse the interpreter arguments.
+     * @param args the interpreter arguments
+     * @return the path to the source file, or null if an error occurred
      */
-    private static void interpret(File sourceFile) {
-        if (Files.notExists(sourceFile.toPath())) {
-            throw new RuntimeException("Couldn't locate source file: " + sourceFile.getAbsolutePath());
+    private static Path parseArguments(String[] args) {
+        boolean success = true;
+        Path path = null;
+        int i;
+        for (i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.startsWith("-")) {
+                success &= parseFlag(arg);
+            } else {
+                if (!arg.endsWith(".leg")) {
+                    arg += ".leg";
+                }
+                path = Paths.get(arg);
+                break;
+            }
         }
+        String[] programArgs = Arrays.copyOfRange(args, i + 1, args.length);
+        return success ? path : null;
+    }
 
-        String fileName = sourceFile.getName();
-        if (!fileName.endsWith(".leg")) {
-            throw new RuntimeException("Couldn't read source file: not a .leg file");
+    private static boolean parseFlag(String flag) {
+        for (int i = 1; i < flag.length(); i++) {
+            switch (flag.charAt(i)) {
+                case 's':
+                    strictTyping = true;
+                    System.err.println("Interpreter flag -s is not yet supported. Proceeding without it...");
+                    break;
+                case 'w':
+                    waitOnExit = true;
+                    break;
+                default:
+                    System.err.printf("Unrecognized interpreter flag '%s'\n", flag.charAt(i));
+                    return false;
+            }
         }
-        String input;
+        return true;
+    }
 
-        try {
-            input = new String(Files.readAllBytes(sourceFile.toPath()));
-        } catch (IOException e) {
-            throw new RuntimeException("Couldn't read source file: " + e.getMessage());
-        }
-
+    private static void interpret(String source) {
         Lexer lexer = new Lexer();
         Parser parser = new Parser();
         Compiler compiler = new Compiler();
 
-        List<TokenLine> tokens = lexer.tokenize(input);
+        List<TokenLine> tokens = lexer.tokenize(source);
         List<Statement> statements = parser.parse(tokens);
         List<Instruction> instructions = compiler.compile(statements);
 
